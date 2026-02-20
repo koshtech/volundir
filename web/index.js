8404a7d3a45abc568f27467be0deddb8c4cbf96a
Fernando Kosh <fernando.kosh@gmail.com>
2026-02-20 00:40:52 -0300

    - Renamed Ingot Template doc folder; - Loading and Registering Service Worker.

--- web/index.js
+++ web/index.js
@@ -1,3 +1,9 @@
+function registerServiceWorker() {
+  navigator.serviceWorker.register('/service-worker.js')
+    .then((reg) => console.log(`Service Worker registered: ${reg}`))
+    .catch((err) => console.warn(`Error registering Service Worker: ${err}`));
+}
+
 let response = await fetch('/lib/core/runtime/core.js', { method: 'HEAD' });
 
 if(response.ok) {
@@ -14,12 +20,26 @@ if(response.ok) {
     }
     const options = {};
 
+    if ('serviceWorker' in navigator) {
+      if (document.readyState === 'complete') {
+        registerServiceWorker();
+      } else {
+        window.addEventListener('load', registerServiceWorker);
+      }
+    }
+
+    if (HTMLScriptElement.supports?.("importmap")) {
+      console.log("Browser supports import maps.");
+    }
+    else {
+      console.warn("Browser does not supports import maps.");
+    }
+
     globalThis.Volundir = Core.create(config, options);
 
     console.log('Volundir: ', Volundir);
 
     // await Volundir.init();
-
   }
 }
 else {
